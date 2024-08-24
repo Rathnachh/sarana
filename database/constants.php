3d5ca960203e0a3af94b6b9f369dc1435596898f
Chansokning <121811513+Chansokning@users.noreply.github.com>
2024-08-24 18:03:57 +0700

    Dynamic Work

--- database/constants.php
+++ database/constants.php
@@ -4,10 +4,7 @@ define("HOST","localhost");
 define("USER","root");
 define("PASS","");
 define("DB","db_sarana");
-<<<<<<< HEAD
 define("DOMAIN","http://localhost:80/sarana");
-
-=======
-define("DOMIAN","http://localhost:80/dormitory/");
->>>>>>> 9bec903e828b8fd83cb8720014cbde975eb17dff
+define("DOMIAN","http://localhost:80/sarana/");
 ?>
+
